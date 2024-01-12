988aa017487200b6b2a59e3e77322e9d38094a1b
Jonnathan Javier Rosero Portilla <jj.rosero@hotmail.com>
2024-01-12 16:30:56 -0500

    Update hello_there.py

--- hello_there.py
+++ hello_there.py
@@ -5,5 +5,6 @@ x = requests.get('https://google.com')
 if x.status_code == 200:
   print("hello docker jenkins web hook!")
   print("test new branch")
+  print("test2")
 else:
   print("bad requests")
